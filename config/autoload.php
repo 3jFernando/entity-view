006dbb5ee5c8c7d6a69ff9f01db6214367bbcd40
fernandogarcia <fernando.claros@misena.edu.co>
2020-10-20 22:48:58 -0500

    ready example

---
+++ config/autoload.php
@@ -0,0 +1,7 @@
+<?php
+
+include __DIR__ . "/../vendor/autoload.php";
+
+$example = new \fernando\entityview\Example();
+
+$example->sayHello("Fernando Claros");
---
+++ src/Example.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace fernando\entityview;
+
+class Example {
+
+  public function sayHello($message)
+  {
+    echo "hola $message \n";
+  }
+
+}
